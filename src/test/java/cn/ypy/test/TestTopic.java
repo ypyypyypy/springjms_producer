fe8f4a4acc708fe2218e8a617af6755838b8e3a0
yangpengyu <ypy540132466@gmail.com>
2019-04-12 16:31:34 +0800

    springjms_producerspring整合activeMQ生产方

---
+++ src/test/java/cn/ypy/test/TestTopic.java
@@ -0,0 +1,24 @@
+package cn.ypy.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import cn.ypy.demo.TopicProducer;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:applicationContext-jms-producer.xml")
+public class TestTopic {
+
+	@Autowired
+	private  TopicProducer topicProducer;
+	
+	@Test
+	public void testSend(){
+		topicProducer.sendTextMessage("spring JMS 发布订阅");
+		
+	}
+	
+}
